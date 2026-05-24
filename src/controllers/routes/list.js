7d1577045c42d777dc818dac27872b0b875baa86
emmabloom04 <blo23006@byui.edu>
2026-05-23 20:25:50 -0600

    finishing filter logic

--- src/controllers/routes/list.js
+++ src/controllers/routes/list.js
@@ -1,14 +1,25 @@
-import { getAllRoutes, getListOfRegions, getListOfSeasons } from '../../models/model.js';
+import { getAllRoutes, getListOfRegions, getListOfSeasons, getRoutesByRegion } from '../../models/model.js';
 
 export default async (req, res) => {
     const regions = await getListOfRegions();
-    const routes = await getAllRoutes();
     const seasons = await getListOfSeasons();
+
+    const region = req.query.region;
+    const season = req.query.season;
+    let routes = await getAllRoutes();
+
+    if (region) {
+        routes = await getRoutesByRegion(region);
+    }
+    if (season) {
+        routes = routes.filter(route => route.bestSeason.toLowerCase() === season.toLowerCase());
+    }
 
     res.render('routes/list', { 
         title: 'Scenic Train Routes',
         regions,
         routes,
-        seasons
+        seasons,
+        query: { region, season }
     });
 };
